c2e670617a89d0cb4ea32dcdc5cb0bc4496c8337
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-03 10:48:28 -0300

    atualizando

--- Project-template-TS-0.0.0/src/migrations/1770125589339-createClass.ts
+++ Project-template-TS-0.0.0/src/migrations/1770125589339-createClass.ts
@@ -1,11 +1,43 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class createClass1770125589339 implements MigrationInterface {
+export default class CreateClass1770125589339 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:'class', 
+                columns:[
+                    {
+                        name:"id",
+                        type:'uuid', 
+                        isPrimary:true,
+                        generationStrategy:'uuid', 
+                        default:'uuid_generate_v4()',
+                    },
+                    {
+                        name:"name",
+                        type:"varchar",
+                    },
+                    {
+                        name:"Duration",
+                        type:"integer", 
+                    },
+                    {
+                        name:"created_At",
+                        type:"timestamp",
+                        default:"now" 
+                    }, 
+                    {
+                        name:"update_At", 
+                        type:"timestamp", 
+                        default:"now()", 
+                    }, 
+                ], 
+            }), 
+        ); 
     }
-
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable('class'); 
     }
 
 }
