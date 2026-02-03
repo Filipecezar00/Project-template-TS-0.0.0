44b8b5ce2e0d078585f35ea505aa855ea06efe6f
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-03 10:35:01 -0300

    atualizando

---
+++ Project-template-TS-0.0.0/src/migrations/1770125589339-createClass.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createClass1770125589339 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
