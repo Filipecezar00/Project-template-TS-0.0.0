a4ee4b05d26cf00f03d1762b4100307a84ab4c9b
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-06 10:27:01 -0300

    atualizando

--- Project-template-TS-0.0.0/src/models/Class.ts
+++ Project-template-TS-0.0.0/src/models/Class.ts
@@ -1,13 +1,22 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 @Entity('class') 
 export class Class{
+    @PrimaryGeneratedColumn("uuid") 
     id:string; 
 
+   @Column({
+    length:100, 
+    unique:true, 
+   })
+ 
     name:string; 
 
+   @Column()
     duration:number; 
 
+   @CreateDateColumn()
     createdAt:Date; 
 
+    @UpdateDateColumn() 
     updateAt:Date; 
 }
