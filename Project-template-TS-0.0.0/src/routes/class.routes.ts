b1b76208b035d893334f809b439cfde7cf54be15
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-06 11:04:37 -0300

    atualizando

---
+++ Project-template-TS-0.0.0/src/routes/class.routes.ts
@@ -0,0 +1,17 @@
+import {Router} from "express"; 
+import { getRepository } from "typeorm"; 
+import {Class} from "../models/Class"; 
+
+const classRouter = Router(); 
+
+classRouter.post("/", async (req,res)=>{
+    try{
+        const repo = getRepository(Class)  
+        const response = await repo.save(req.body); 
+        return res.status(201).json(response) 
+    }catch(error){
+        res.status(404).json(error) 
+    }
+})
+
+export default classRouter 
